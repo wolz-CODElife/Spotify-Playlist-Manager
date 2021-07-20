99e4cd21570d4bc64d68be0186c594960487e19e
wolz-CODElife <joeladewole3@gmail.com>
2021-07-20 05:58:00 +0100

    Added user profile

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { useState} from 'react'
 import { Link } from 'react-router-dom'
 import userImg from '../../assets/justin.PNG'
 
-const NavBar = ({userData}) => {
+const NavBar = ({ userData }) => {
+    const [userProfile, setUserProfile] = useState(false)
     return (
         <>
-            <div style={{ margin: "1rem 0.5rem", display: 'flex', justifyContent: 'space-between' }}>
-                <div className="dropDown">                    
-                    <img src="" alt="" />
-                    <ul>
-                        <li>Name: </li>
-                        <li>Email: </li>
-                        <li>Profile >> </li>
-                    </ul>
+            <div style={{ margin: "2rem 1rem", marginBottom: '0rem', display: 'flex', justifyContent: 'space-between' }}>
+                <div
+                    className="dropDown"
+                    onMouseEnter={() => setUserProfile(!userProfile)}
+                    onMouseLeave={() => setUserProfile(false)}>
+                    <img src={userImg} alt="user" />
+                    {userProfile && <ul>
+                        <li><h3>{ userData.name || 'John Doe' }</h3></li>
+                        <li>
+                            <p style={{ margin: '0px' }}>
+                                <a href={`mailto:${userData.email || 'johndoe@gmail.com'}`}>johndoe@gmail.com</a>
+                            </p>
+                        </li>
+                        <li>
+                            <p style={{ margin: '0px' }}>
+                                <a href={userData.url || '/'} target="_blank" rel="noopener noreferrer">Profile >></a>
+                            </p>
+                        </li>
+                    </ul>}
                 </div>
                 <Link to="/" className="btn">Home</Link>
             </div>
